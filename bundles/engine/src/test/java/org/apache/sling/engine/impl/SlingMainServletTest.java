7112e8bead1043b23a3b0245f2c09bf9e0d60d1c
Petr Shypila <ikrumping@gmail.com>
2015-07-01 21:42:12 +0200

    New tests for SlingMainServlet

--- bundles/engine/src/test/java/org/apache/sling/engine/impl/SlingMainServletTest.java
+++ bundles/engine/src/test/java/org/apache/sling/engine/impl/SlingMainServletTest.java
@@ -19,15 +19,13 @@
 package org.apache.sling.engine.impl;
 
 import junitx.util.PrivateAccessor;
-import org.apache.sling.api.servlets.ServletResolver;
+import org.apache.sling.api.request.SlingRequestEvent;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.auth.core.AuthenticationSupport;
-import org.apache.sling.engine.SlingRequestProcessor;
-import org.apache.sling.engine.impl.filter.ServletFilterManager;
 import org.apache.sling.engine.impl.helper.RequestListenerManager;
-import org.apache.sling.engine.impl.helper.SlingServletContext;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import org.apache.sling.testing.mock.sling.junit.SlingContext;
-import org.apache.sling.testing.mock.sling.servlet.MockServletContext;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletResponse;
 import org.junit.Before;
@@ -35,50 +33,117 @@ import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class SlingMainServletTest {
     private SlingMainServlet underTest;
+    private ResourceResolver resourceResolver;
 
     @Rule
     public final SlingContext context = new SlingContext(ResourceResolverType.JCR_JACKRABBIT);
 
     @Before
-    public void setup(){
+    public void setup() throws LoginException {
         underTest = new SlingMainServlet();
+        resourceResolver = context.resourceResolver().clone(null);
     }
 
     @Test
-    public void testService() throws ServletException, NoSuchFieldException, IOException {
-        final SlingServletContext servletContext = new SlingServletContext(context.bundleContext(), underTest);
-        final RequestListenerManager r = new RequestListenerManager(context.bundleContext(), servletContext);
-        PrivateAccessor.setField(underTest, "requestListenerManager", r);
+    public void testServiceWithGetRequest() throws ServletException, NoSuchFieldException, IOException {
+        final RequestListenerManager requestListenerManager = mock(RequestListenerManager.class);
+        PrivateAccessor.setField(underTest, "requestListenerManager", requestListenerManager);
 
-
-        ServletFilterManager filterManager = new ServletFilterManager(context.bundleContext(), servletContext, true);
-        SlingRequestProcessor requestProcessor = (SlingRequestProcessor) PrivateAccessor.getField(underTest, "requestProcessor");
-        PrivateAccessor.setField(requestProcessor, "filterManager", filterManager);
-
-        ServletResolver resolver = mock(ServletResolver.class);
-
-        underTest.setServletResolver(resolver);
+        final SlingRequestProcessorImpl requestProcessor = mock(SlingRequestProcessorImpl.class);
+        PrivateAccessor.setField(underTest, "requestProcessor", requestProcessor);
 
         final MockSlingHttpServletRequest req = spy(context.request());
-        req.setAttribute(AuthenticationSupport.REQUEST_ATTRIBUTE_RESOLVER, context.resourceResolver());
-
-        final MockSlingHttpServletResponse res = spy(context.response());
+        req.setMethod("GET");
+        req.setAttribute(AuthenticationSupport.REQUEST_ATTRIBUTE_RESOLVER, resourceResolver);
 
         doReturn("127.0.0.1").when(req).getRemoteAddr();
         doReturn("HTTP/1.1").when(req).getProtocol();
         doReturn("/").when(req).getRequestURI();
-        doReturn("/").when(req).getServletPath();
-        doReturn(null).when(req).getPathInfo();
 
+        final MockSlingHttpServletResponse res = spy(context.response());
+
+        assertNotEquals(405, res.getStatus());
+        assertNotEquals("GET, HEAD, POST, PUT, DELETE, OPTIONS", res.getHeader("Allow"));
         underTest.service(req, res);
+        //Could be set to these values only if request's method is TRACE, not GET
+        assertNotEquals(405, res.getStatus());
+        assertNotEquals("GET, HEAD, POST, PUT, DELETE, OPTIONS", res.getHeader("Allow"));
+
+        //Checking that all required methods where called with correct parameters.
+        verify(requestProcessor, times(1)).doProcessRequest(req, res, resourceResolver);
+        verify(requestListenerManager, times(1)).sendEvent(req, SlingRequestEvent.EventType.EVENT_INIT);
+        verify(requestListenerManager, times(1)).sendEvent(req, SlingRequestEvent.EventType.EVENT_DESTROY);
+    }
+
+    @Test
+    public void testServiceWithTraceRequest() throws ServletException, NoSuchFieldException, IOException {
+        final RequestListenerManager requestListenerManager = mock(RequestListenerManager.class);
+        PrivateAccessor.setField(underTest, "requestListenerManager", requestListenerManager);
+
+        final SlingRequestProcessorImpl requestProcessor = mock(SlingRequestProcessorImpl.class);
+        PrivateAccessor.setField(underTest, "requestProcessor", requestProcessor);
+
+        final MockSlingHttpServletRequest req = spy(context.request());
+        req.setMethod("TRACE");
+        req.setAttribute(AuthenticationSupport.REQUEST_ATTRIBUTE_RESOLVER, resourceResolver);
+
+        doReturn("127.0.0.1").when(req).getRemoteAddr();
+        doReturn("HTTP/1.1").when(req).getProtocol();
+        doReturn("/").when(req).getRequestURI();
+
+        final MockSlingHttpServletResponse res = spy(context.response());
+
+        assertNotEquals(405, res.getStatus());
+        assertNotEquals("GET, HEAD, POST, PUT, DELETE, OPTIONS", res.getHeader("Allow"));
+        underTest.service(req, res);
+        //Should be set to these values because of TRACE method
+        assertEquals(405, res.getStatus());
+        assertEquals("GET, HEAD, POST, PUT, DELETE, OPTIONS", res.getHeader("Allow"));
+
+        //Checking that requestProcessor was not called because of request's method type.
+        verify(requestProcessor, times(0)).doProcessRequest(req, res, resourceResolver);
+        verify(requestListenerManager, times(1)).sendEvent(req, SlingRequestEvent.EventType.EVENT_INIT);
+        verify(requestListenerManager, times(1)).sendEvent(req, SlingRequestEvent.EventType.EVENT_DESTROY);
+    }
+
+    @Test
+    public void testServiceWithoutResourceResolver() throws ServletException, NoSuchFieldException, IOException {
+        final RequestListenerManager requestListenerManager = mock(RequestListenerManager.class);
+        PrivateAccessor.setField(underTest, "requestListenerManager", requestListenerManager);
+
+        final SlingRequestProcessorImpl requestProcessor = mock(SlingRequestProcessorImpl.class);
+        PrivateAccessor.setField(underTest, "requestProcessor", requestProcessor);
+
+        final MockSlingHttpServletRequest req = spy(context.request());
+        req.setMethod("GET");
+        doReturn("127.0.0.1").when(req).getRemoteAddr();
+        doReturn("HTTP/1.1").when(req).getProtocol();
+        doReturn("/").when(req).getRequestURI();
+
+        final MockSlingHttpServletResponse res = spy(context.response());
+
+        assertNotEquals(405, res.getStatus());
+        assertNotEquals("GET, HEAD, POST, PUT, DELETE, OPTIONS", res.getHeader("Allow"));
+        underTest.service(req, res);
+        //There we checking that response object was not changed inside this method.
+        //Below are the only values which could be set inside SlingMainServlet#service(...) method
+        assertNotEquals(405, res.getStatus());
+        assertNotEquals("GET, HEAD, POST, PUT, DELETE, OPTIONS", res.getHeader("Allow"));
+
+        //Checking that all required methods where called with correct parameters.
+        verify(requestProcessor, times(1)).doProcessRequest(req, res, null);
+        verify(requestListenerManager, times(1)).sendEvent(req, SlingRequestEvent.EventType.EVENT_INIT);
+        verify(requestListenerManager, times(1)).sendEvent(req, SlingRequestEvent.EventType.EVENT_DESTROY);
     }
 }
